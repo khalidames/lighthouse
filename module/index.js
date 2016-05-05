bfc8be83db80953f1fa7b5976d260620802cd0ac
Paul Lewis <github@aerotwist.com>
2016-05-05 16:05:04 +0100

    Fixes comments

--- module/index.js
+++ module/index.js
@@ -42,13 +42,5 @@ module.exports = function(url, flags) {
   }
 
   // kick off a lighthouse run
-  return lighthouse(driver, {url, flags})
-    .catch(err => {
-      if (err.code === 'ECONNREFUSED') {
-        console.error('Unable to connect to Chrome. Did you run ./launch-chrome.sh?');
-      } else {
-        console.error('Runtime error encountered:', err);
-        console.error(err.stack);
-      }
-    });
+  return lighthouse(driver, {url, flags});
 };
