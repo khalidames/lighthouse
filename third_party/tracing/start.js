89f6fa28b2727d34223d9ac4fcbd09555f4a9dec
Paul Lewis <github@aerotwist.com>
2016-05-09 22:41:59 +0100

    Adds third_party

---
+++ third_party/tracing/start.js
@@ -0,0 +1,9 @@
+/**
+Copyright (c) 2014 The Chromium Authors. All rights reserved.
+Use of this source code is governed by a BSD-style license that can be
+found in the LICENSE file.
+**/
+
+require('./third_party/tracing/importer/import.js');
+require('./third_party/tracing/model/model.js');
+require('./third_party/tracing/extras/full_config.js');
