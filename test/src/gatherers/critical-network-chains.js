2f2a781fa2160cfaaa8a078ed5e282e21d433ee5
Paul Lewis <github@aerotwist.com>
2016-05-26 10:24:01 +0100

    Fixes tests

--- test/src/gatherers/critical-network-chains.js
+++ test/src/gatherers/critical-network-chains.js
@@ -17,52 +17,9 @@
 
 /* eslint-env mocha */
 
-const GathererClass = require('../../../src/gatherers/critical-network-chains');
+const GathererClass = require('../../../src/gatherers/critical-request-chains');
 const assert = require('assert');
-
 const Gatherer = new GathererClass();
-
-// We'll have to pull a lot of shenanigans becase assert.deepEqual does not
-// properly support ES6 sets yet (see github.com/nodejs/node/issues/2309)
-
-/**
- * Boolean version of assert.deepEqual
- * Obviously this doesn't work on ES6 sets
- */
-function isDeepEqual(obj1, obj2) {
-  try {
-    assert.deepEqual(obj1, obj2);
-    return true;
-  } catch (e) {
-    return false;
-  }
-}
-
-function deepEqualSet(set1, set2) {
-  if (set1.size !== set2.size) {
-    return false;
-  }
-
-  const set2Copy = new Set([...set2.values()]);
-  // Horrible horrible O(n^2) check. I assume the max size of sets will be
-  // 10 and thus hereby declare this O(1).
-  for (let elm of set1.values()) {
-    let found = false;
-    for (let set2elm of set2Copy.values()) {
-      if (isDeepEqual(elm, set2elm)) {
-        found = true;
-        // Tiny optimization
-        set2Copy.delete(set2elm);
-        break;
-      }
-    }
-    if (!found) {
-      return false;
-    }
-  }
-
-  return true;
-}
 
 const HIGH = 'High';
 const VERY_HIGH = 'VeryHigh';
@@ -72,117 +29,247 @@ const VERY_LOW = 'VeryLow';
 
 function mockTracingData(prioritiesList, edges) {
   const networkRecords = prioritiesList.map((priority, index) =>
-      ({requestId: index,
+      ({requestId: index.toString(),
         initialPriority: () => priority,
         initiatorRequest: () => null}));
 
-  // Add a high priority request at the beginning - that's the first webpage req
-  const firstNetworkRequest = {
-    requestId: '-1',
-    initialPriority: () => VERY_HIGH,
-    initiatorRequest: () => null
-  };
-
   // add mock initiator information
   edges.forEach(edge => {
-    networkRecords[edge[1]].initiatorRequest = () => ({requestId: edge[0]});
+    const initiator = networkRecords[edge[0]];
+    networkRecords[edge[1]].initiatorRequest = () => initiator;
   });
 
-  return [firstNetworkRequest].concat(networkRecords);
+  return networkRecords;
 }
 
 function testGetCriticalChain(data) {
-  const mockNetworkRecords = mockTracingData(data.priorityList, data.edges);
-  const criticalChains = Gatherer.getCriticalChains(mockNetworkRecords);
-  // It is sufficient to only check the requestIds are correct in the chain
-  const requestIdChains = criticalChains.map(chain =>
-    chain.map(node => node.requestId));
-  // Ordering of the chains do not matter
-  assert(deepEqualSet(new Set(requestIdChains), new Set(data.expectedChains)));
+  const networkRecords = mockTracingData(data.priorityList, data.edges);
+  Gatherer.postProfiling(null, {networkRecords});
+  const criticalChains = Gatherer.artifact;
+  assert.deepEqual(criticalChains, data.expected);
 }
 
-describe('CriticalNetworkChain gatherer: getCriticalChain function', () => {
-  describe('Meta Testing: deepEqualSet', () => {
-    it('says equal sets are equal', () => {
-      const a = new Set([1, 2, 3]);
-      const b = new Set([1, 3, 2]);
-      assert(deepEqualSet(a, b));
-    });
+function constructEmptyRequest() {
+  return {
+    endTime: undefined,
+    responseReceivedTime: undefined,
+    startTime: undefined,
+    url: undefined
+  };
+}
 
-    it('says deeply equal sets are equal', () => {
-      const a = new Set([[0, 1], [1, 2], [2, 3]]);
-      const b = new Set([[0, 1], [2, 3], [1, 2]]);
-      assert(deepEqualSet(a, b));
-    });
-
-    it('says unequal sets are unequal', () => {
-      const a = new Set([1, 2, 3]);
-      const b = new Set([1, 2]);
-      assert(!deepEqualSet(a, b));
-    });
-  });
-
+describe('CriticalRequestChain gatherer: getCriticalChain function', () => {
   it('returns correct data for chain of four critical requests', () =>
     testGetCriticalChain({
       priorityList: [HIGH, MEDIUM, VERY_HIGH, HIGH],
       edges: [[0, 1], [1, 2], [2, 3]],
-      expectedChains: [[0, 1, 2, 3]]
+      expected: {
+        criticalNetworkChains: {
+          0: {
+            request: constructEmptyRequest(),
+            children: {
+              1: {
+                request: constructEmptyRequest(),
+                children: {
+                  2: {
+                    request: constructEmptyRequest(),
+                    children: {
+                      3: {
+                        request: constructEmptyRequest(),
+                        children: {}
+                      }
+                    }
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
     }));
 
   it('returns correct data for chain interleaved with non-critical requests',
     () => testGetCriticalChain({
       priorityList: [MEDIUM, HIGH, LOW, MEDIUM, HIGH, VERY_LOW],
       edges: [[0, 1], [1, 2], [2, 3], [3, 4]],
-      expectedChains: [[0, 1], [3, 4]]
+      expected: {
+        criticalNetworkChains: {
+          0: {
+            request: constructEmptyRequest(),
+            children: {
+              1: {
+                request: constructEmptyRequest(),
+                children: {}
+              }
+            }
+          }
+        }
+      }
     }));
 
   it('returns correct data for two parallel chains', () =>
     testGetCriticalChain({
       priorityList: [HIGH, HIGH, HIGH, HIGH],
       edges: [[0, 2], [1, 3]],
-      expectedChains: [[1, 3], [0, 2]]
+      expected: {
+        criticalNetworkChains: {
+          0: {
+            request: constructEmptyRequest(),
+            children: {
+              2: {
+                request: constructEmptyRequest(),
+                children: {}
+              }
+            }
+          },
+          1: {
+            request: constructEmptyRequest(),
+            children: {
+              3: {
+                request: constructEmptyRequest(),
+                children: {}
+              }
+            }
+          }
+        }
+      }
     }));
 
   it('returns correct data for fork at root', () =>
     testGetCriticalChain({
       priorityList: [HIGH, HIGH, HIGH],
       edges: [[0, 1], [0, 2]],
-      expectedChains: [[0, 1], [0, 2]]
+      expected: {
+        criticalNetworkChains: {
+          0: {
+            request: constructEmptyRequest(),
+            children: {
+              1: {
+                request: constructEmptyRequest(),
+                children: {}
+              },
+              2: {
+                request: constructEmptyRequest(),
+                children: {}
+              }
+            }
+          }
+        }
+      }
     }));
 
   it('returns correct data for fork at non root', () =>
     testGetCriticalChain({
       priorityList: [HIGH, HIGH, HIGH, HIGH],
       edges: [[0, 1], [1, 2], [1, 3]],
-      expectedChains: [[0, 1, 2], [0, 1, 3]]
+      expected: {
+        criticalNetworkChains: {
+          0: {
+            request: constructEmptyRequest(),
+            children: {
+              1: {
+                request: constructEmptyRequest(),
+                children: {
+                  2: {
+                    request: constructEmptyRequest(),
+                    children: {}
+                  },
+                  3: {
+                    request: constructEmptyRequest(),
+                    children: {}
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
     }));
 
   it('returns empty chain list when no critical request', () =>
     testGetCriticalChain({
       priorityList: [LOW, LOW],
       edges: [[0, 1]],
-      expectedChains: []
+      expected: {
+        criticalNetworkChains: {}
+      }
     }));
 
   it('returns empty chain list when no request whatsoever', () =>
     testGetCriticalChain({
       priorityList: [],
       edges: [],
-      expectedChains: []
+      expected: {
+        criticalNetworkChains: {}
+      }
     }));
 
   it('returns two single node chains for two independent requests', () =>
     testGetCriticalChain({
       priorityList: [HIGH, HIGH],
       edges: [],
-      expectedChains: [[0], [1]]
+      expected: {
+        criticalNetworkChains: {
+          0: {
+            request: constructEmptyRequest(),
+            children: {}
+          },
+          1: {
+            request: constructEmptyRequest(),
+            children: {}
+          }
+        }
+      }
     }));
 
   it('returns correct data on a random big graph', () =>
     testGetCriticalChain({
       priorityList: Array(9).fill(HIGH),
       edges: [[0, 1], [1, 2], [1, 3], [4, 5], [5, 7], [7, 8], [5, 6]],
-      expectedChains: [
-        [0, 1, 2], [0, 1, 3], [4, 5, 7, 8], [4, 5, 6]
-      ]}));
+      expected: {
+        criticalNetworkChains: {
+          0: {
+            request: constructEmptyRequest(),
+            children: {
+              1: {
+                request: constructEmptyRequest(),
+                children: {
+                  2: {
+                    request: constructEmptyRequest(),
+                    children: {}
+                  },
+                  3: {
+                    request: constructEmptyRequest(),
+                    children: {}
+                  }
+                }
+              }
+            }
+          },
+          4: {
+            request: constructEmptyRequest(),
+            children: {
+              5: {
+                request: constructEmptyRequest(),
+                children: {
+                  7: {
+                    request: constructEmptyRequest(),
+                    children: {
+                      8: {
+                        request: constructEmptyRequest(),
+                        children: {}
+                      }
+                    }
+                  },
+                  6: {
+                    request: constructEmptyRequest(),
+                    children: {}
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    }));
 });
